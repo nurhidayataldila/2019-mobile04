0e550be14598d5b9080aab36247651f6277ba490
Nurhidayat Aldila <nurhidayat.aldila@gmail.com>
2019-10-01 13:37:54 +0700

    Melengkapi Parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -2,14 +2,39 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import id.ac.polinema.intent.model.User;
 
 public class ParcelableActivity extends AppCompatActivity {
+
+    public static final String USERNAME_KEY = "username";
+
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
+    }
+
+    public void handleSubmit(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+
+        User user = new User(username, name, age);
+        Intent intent = new Intent(this, ProfileBundleActivity.class);
+
+        intent.putExtra(USERNAME_KEY, username);
+        startActivity(intent);
     }
 
 }
